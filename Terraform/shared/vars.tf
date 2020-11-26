28183c415a1322248e03e243512f4e7704f9da11
firewall413 <60637730+firewall413@users.noreply.github.com>
2020-11-26 20:36:00 +1100

    added basic infra: VPC, SG, SN, ECS

---
+++ Terraform/shared/vars.tf
@@ -0,0 +1,25 @@
+variable "public_key_path" {
+  description = "Path to the SSH public key to be used for authentication."
+  default = "/c:/Users/Administrator/.ssh/ddpm_keypair_arno.pub"
+}
+
+variable "private_key_path" {
+  description = "Path to the SSH private key to be used for authentication."
+  default = "/c:/Users/Administrator/.ssh/ddpm_keypair_arno.pem"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair."
+  default = "ddpm_keypair_arno"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "ap-southeast-2"
+}
+
+variable "aws_zones" {
+ type        = list
+ description = "List of availability zones to use"
+ default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+}
